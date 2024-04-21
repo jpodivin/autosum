8002c4c083e4359d22ab0b26ef57ccf64b0e7f01
Jiri Podivin <jpodivin@gmail.com>
2024-04-21 22:07:42 +0200

    We retriver now
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

--- autosum/summarize.py
+++ autosum/summarize.py
@@ -3,98 +3,81 @@
 from transformers import pipeline
 from argparse import ArgumentParser
 import logging
-import textwrap
 import sys
-import pypdf
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader, UnstructuredMarkdownLoader, JSONLoader, CSVLoader, TextLoader
+from langchain_community.embeddings import GPT4AllEmbeddings
 
 LOGGER = logging.getLogger("default")
 
 
-def summarize_text(summarizer, text, split_size=300, min_tokens=200,
-                   max_context=None, max_summary_size=1000):
-
-    if not max_context:  # Auto set max_context from tokenizer property
-        max_context = summarizer.tokenizer.max_len_single_sentence
-
-    if split_size - 10 <= 0:
-        LOGGER.warning(
-          "minimum split size reached, your file probably includes long continuous strings"
-          "these can't be effectivelly summarized without information loss")
-        return text
-
-    ntokens = len(summarizer.tokenizer(text)['input_ids'])
-
-    if ntokens > max_context:  # If we can't fit in context split into smaller chunks
-        LOGGER.info(f"max context {max_context} exceeded, going deeper!")
-        text = text.split()
-        samples = [summarize_text(summarizer, ' '.join(text[i:i+split_size]),
-                                  split_size-10)
-                   for i in range(0, len(text), split_size)]
-        if len(samples) > max_summary_size:
-            return summarize_text(summarizer, ' '.join(samples), split_size)
-        return ' '.join(samples)
-    elif ntokens < min_tokens:
-        return text
+def get_doc(path: str) -> list:
+    if path.endswith('.pdf'):
+        loader = PyPDFLoader(path, extract_images=True)
+    elif path.endswith('.csv'):
+        loader = CSVLoader(path)
+    elif path.endswith('.json'):
+        loader = JSONLoader(path)
+    elif path.endswith('.md'):
+        loader = UnstructuredMarkdownLoader(path)
     else:
-        return summarizer(text)[0]['summary_text']
+        loader = TextLoader(path)
+    return loader.load()
+
+
+def search_doc(query: str, database):
+
+    return database.similarity_search(query, k=1)
+
+
+def parse_doc(segments: list):
+
+    faiss_index = FAISS.from_documents(segments, GPT4AllEmbeddings())
+    return faiss_index
+
+
+TEST_PATH = "~/Documents/contractRedHat.pdf"
+TEST_QUERY = "salary"
 
 
 def main():
 
-    parser = ArgumentParser(
-        prog="summarize",
-        description="Summarize arbitrary text files using LLM")
+    # parser = ArgumentParser(
+    #     prog="retrieve",
+    #     description="Retrieve info form arbitrary text files using LLM")
 
-    parser.add_argument('--document', '-d', type=str, default=None)
+    # parser.add_argument('query', type=str)
+    # parser.add_argument('--document', '-d', type=str, default=None)
 
-    # https://huggingface.co/Falconsai/text_summarization Apache 2.0 license
-    parser.add_argument('--model', '-m',
-                        type=str, default='Falconsai/text_summarization',
-                        help='Hugging face signature of your chosen model')
-    parser.add_argument('--max-size', '-S',
-                        type=int, default=1000,
-                        help='Maximum size of summarized document')
-    parser.add_argument('--summary-width', '-C', type=int, default=80,
-                        help='Width of summary output in columns')
-    parser.add_argument('-v', '--verbose', action='store_true')
+    # # https://huggingface.co/Falconsai/text_summarization Apache 2.0 license
+    # parser.add_argument('--model', '-m',
+    #                     type=str, default='Falconsai/text_summarization',
+    #                     help='Hugging face signature of your chosen model')
+    # parser.add_argument('-v', '--verbose', action='store_true')
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
-    document = ""
+    # document = ""
 
-    if args.verbose:
-        LOGGER.setLevel(logging.DEBUG)
-    summarizer = pipeline('summarization', model=args.model)
-    try:
-        if not args.document:
-            document = '\n'.join(sys.stdin.readlines())
-        else:
-            if args.document.endswith('.pdf'):
-                reader = pypdf.PdfReader(args.document)
-                for page in reader.pages:
-                    document += f"\n {page.extract_text()}"
-            else:
-                with open(args.document, 'r', encoding='utf-8') as f:
-                    document = f.read()
-    except UnicodeDecodeError:
-        LOGGER.error("Document decoding failed. Check your encoding!",
-                      exc_info=True)
-        exit(1)
+    # if args.verbose:
+    #     LOGGER.setLevel(logging.DEBUG)
+    # try:
+    #     if not args.document:
+    #         document = '\n'.join(sys.stdin.readlines())
+    #     else:
+    #         document = get_doc(args.document)
+    # except UnicodeDecodeError:
+    #     LOGGER.error("Document decoding failed. Check your encoding!",
+    #                  exc_info=True)
+    #     exit(1)
+    document = get_doc(TEST_PATH)
+    database = parse_doc(document)
 
-    doc_lines = document.split('\n')
-
-    LOGGER.info(
-        f"Original -> lines: {len(doc_lines)} words: {len(document.split(' '))} chars {len(document)}")
-
-    summary = summarize_text(summarizer, document,
-                             max_summary_size=args.max_size)
-
-    lines = summary.split('\n')
-    LOGGER.info(f"Summarized -> lines: {len(lines)} words: {len(summary.split())} chars {len(summary)}")
-    summary = '\n'.join(textwrap.wrap(summary, width=80))
-
-    print(summary)
-
+    results = search_doc(TEST_QUERY, database=database)
+    summarizer = pipeline('summarization', model='Falconsai/text_summarization')
+    for r in results:
+        print(summarizer(r.page_content)[0]['summary_text'])
+        #print(str(r.metadata["page"]) + ":", r.page_content)
 
 if __name__ == '__main__':
     main()

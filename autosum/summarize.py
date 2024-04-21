2051b15422bc8256948543def3f46fe5180d090a
Jiri Podivin <jpodivin@gmail.com>
2024-04-21 14:17:28 +0200

    Reindentation and style cleanup
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

--- autosum/summarize.py
+++ autosum/summarize.py
@@ -1,8 +1,7 @@
 #!/bin/env python
 
-from transformers import pipeline, AutoTokenizer
+from transformers import pipeline
 from argparse import ArgumentParser
-import os
 import logging
 import textwrap
 import sys
@@ -10,31 +9,35 @@ import pypdf
 
 LOGGER = logging.getLogger("default")
 
-def summarize_text(summarizer, text, split_size = 300, min_tokens=200, max_context=None, max_summary_size=1000):
-  
-  if not max_context: # Auto set max_context from tokenizer property
-    max_context = summarizer.tokenizer.max_len_single_sentence
 
-  if split_size - 10 <= 0:
-    LOGGER.warning(
-      "minimum split size reached, your file probably includes long continuous strings"
-      "these can't be effectivelly summarized without information loss")
-    return text
+def summarize_text(summarizer, text, split_size=300, min_tokens=200,
+                   max_context=None, max_summary_size=1000):
 
-  ntokens = len(summarizer.tokenizer(text)['input_ids'])
+    if not max_context:  # Auto set max_context from tokenizer property
+        max_context = summarizer.tokenizer.max_len_single_sentence
 
-  if ntokens > max_context: # If we can't fit in context split into smaller chunks
-    LOGGER.info(f"max context {max_context} exceeded, going deeper!")
-    text = text.split()
-    samples = [summarize_text(summarizer, ' '.join(text[i:i+split_size]), split_size-10)
-      for i in range(0, len(text), split_size)]
-    if len(samples) > max_summary_size:
-      return summarize_text(summarizer, ' '.join(samples), split_size)
-    return ' '.join(samples)
-  elif ntokens < min_tokens:
-    return text
-  else:
-    return summarizer(text)[0]['summary_text']
+    if split_size - 10 <= 0:
+        LOGGER.warning(
+          "minimum split size reached, your file probably includes long continuous strings"
+          "these can't be effectivelly summarized without information loss")
+        return text
+
+    ntokens = len(summarizer.tokenizer(text)['input_ids'])
+
+    if ntokens > max_context:  # If we can't fit in context split into smaller chunks
+        LOGGER.info(f"max context {max_context} exceeded, going deeper!")
+        text = text.split()
+        samples = [summarize_text(summarizer, ' '.join(text[i:i+split_size]),
+                                  split_size-10)
+                   for i in range(0, len(text), split_size)]
+        if len(samples) > max_summary_size:
+            return summarize_text(summarizer, ' '.join(samples), split_size)
+        return ' '.join(samples)
+    elif ntokens < min_tokens:
+        return text
+    else:
+        return summarizer(text)[0]['summary_text']
+
 
 def main():
 
@@ -74,20 +77,24 @@ def main():
                 with open(args.document, 'r', encoding='utf-8') as f:
                     document = f.read()
     except UnicodeDecodeError:
-        LOGGER.error("Document decoding failed. Check your encoding!", exc_info=True)
+        LOGGER.error("Document decoding failed. Check your encoding!",
+                      exc_info=True)
         exit(1)
 
     doc_lines = document.split('\n')
 
-    LOGGER.info(f"Original -> lines: {len(doc_lines)} words: {len(document.split(' '))} chars {len(document)}")
+    LOGGER.info(
+        f"Original -> lines: {len(doc_lines)} words: {len(document.split(' '))} chars {len(document)}")
 
-    summary = summarize_text(summarizer, document, max_summary_size=args.max_size)
+    summary = summarize_text(summarizer, document,
+                             max_summary_size=args.max_size)
 
     lines = summary.split('\n')
     LOGGER.info(f"Summarized -> lines: {len(lines)} words: {len(summary.split())} chars {len(summary)}")
     summary = '\n'.join(textwrap.wrap(summary, width=80))
 
     print(summary)
+
 
 if __name__ == '__main__':
     main()
